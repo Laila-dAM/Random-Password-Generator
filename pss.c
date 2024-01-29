a0288c709571a44ede5208458b78cbcaedc6ce16
Laila-dAM <lailamota.238@gmail.com>
2024-01-29 18:29:45 -0300

    Sets a limit of 100 characters and generates random password with uppercase, lowercase, numbers and special options

--- pss.c
+++ pss.c
@@ -2,3 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
+
+#define MAX_LENGTH 100
+
+void generatePassword (int length, int includeUppercase, int includeLowercase, int includeNumbers, int includeSpecialChars) {
+    
+}
